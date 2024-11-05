e1e508c6665373b5d60ad384fb8b99a2ca87f30b
{tknohas} <0xv17851726325v@gmail.com>
2024-11-05 10:46:19 +0900

    画像が表示されるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root "home#index"
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
-
-  root "home#index"
 end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Products', type: :system do
       expect(page).to have_css 'h1', text: '商品一覧'
       expect(page).to have_content 'にんじん'
       expect(page).to have_content '1,000円'
+      expect(page).to have_css 'img.product-image'
     end
   end
 end
