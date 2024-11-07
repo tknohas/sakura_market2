70ccdbc5d28d5d355967f29fc0d95a053eda8d2c
{tknohas} <0xv17851726325v@gmail.com>
2024-11-07 17:44:29 +0900

    公開設定条件分岐追加

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe 'Products', type: :system do
         visit product_path(product)
 
         expect(page).to have_css 'h1', text: '商品詳細'
+        expect(page).to have_no_content '現在、この商品は公開されていません。'
         expect(page).to have_content 'にんじん'
         expect(page).to have_content '1,000円'
         expect(page).to have_content '商品説明です。'
