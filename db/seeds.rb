af1f77e9593779fb694971f7153c936393142962
{tknohas} <0xv17851726325v@gmail.com>
2024-11-07 14:14:12 +0900

    管理者がログインできるように

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ products = %w(にんじん 玉ねぎ ピーマン ジャガイモ 白菜 大根)
   Product.create!(name: products[n - 1], price: n * 1000, description: "#{n}番目の商品です。", private: false, position: n)
 end
 User.create!(name: 'Alice', email: 'alice@example.com', password: '123456')
+Admin.create!(email: 'admin@example.com', password: '123456')
---
+++ spec/system/admins_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe 'Admins', type: :system do
+  describe 'ログイン' do
+    let!(:admin) { create(:admin, email: 'admin@example.com', password: '123456') }
+
+    before do
+      visit new_admin_session_path
+    end
+
+    context 'フォームの入力値が正常' do
+      it 'ログイン成功' do
+        fill_in 'admin_email', with: 'admin@example.com'
+        fill_in 'admin_password', with: '123456'
+        within '.actions' do
+          click_button 'ログイン'
+        end
+
+        expect(page).to have_content 'ログインしました。'
+      end
+    end
+
+    context 'フォームの入力値が異常' do
+      it 'ログイン失敗' do
+        fill_in 'admin_email', with: ''
+        fill_in 'admin_password', with: ''
+        within '.actions' do
+          click_button 'ログイン'
+        end
+
+        expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+      end
+    end
+  end
+end
