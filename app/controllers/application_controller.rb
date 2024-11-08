8a09e5533b5108be3f908e5799e68282c96f783d
{tknohas} <0xv17851726325v@gmail.com>
2024-11-08 17:57:18 +0900

    セッションのカートの情報を引き継げるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   def current_cart
     if user_signed_in?
       current_cart = current_user.cart || current_user.create_cart!
+      current_cart.merge_guest_cart_items(session[:cart_id]) if session[:cart_id]
+      session.delete(:cart_id)
     else
       current_cart = Cart.find_or_create_by!(id: session[:cart_id])
       session[:cart_id] ||= current_cart.id
