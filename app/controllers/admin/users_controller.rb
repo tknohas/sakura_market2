d1ab86c0bcc367b9136077bde241d0b184450809
{tknohas} <0xv17851726325v@gmail.com>
2024-11-08 14:43:59 +0900

    管理者がユーザーを編集できるように

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,27 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def index
     @users = User.order(:id)
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: '変更しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
