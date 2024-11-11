b9637d4a22c191ca01d98aebb62b9bc08ffd01fc
{tknohas} <0xv17851726325v@gmail.com>
2024-11-11 09:47:08 +0900

    order指定

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,6 +5,8 @@ class CartItem < ApplicationRecord
   validates :cart_id, uniqueness: { scope: :product_id }
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  scope :order_by_created_at, -> { order(:created_at) }
+
   def item_total
     product.price * amount
   end
